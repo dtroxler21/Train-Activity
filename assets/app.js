2288cd97e2b655b9e1c907dc5fcd45a917a41225
dtroxler21 <dtroxler21@gmail.com>
2018-03-02 08:42:51 -0500

    create html and initialize firebase

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCMyCHVnUVvanSquGD6tZHq1ajc-TpaCFk",
+    authDomain: "train-7707c.firebaseapp.com",
+    databaseURL: "https://train-7707c.firebaseio.com",
+    projectId: "train-7707c",
+    storageBucket: "",
+    messagingSenderId: "688712147615"
+  };
+  firebase.initializeApp(config);
