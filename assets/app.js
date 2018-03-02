4f0c322ea46a1a71e674c0a2f679945013a852c0
dtroxler21 <dtroxler21@gmail.com>
2018-03-02 09:06:23 -0500

    add train event and firebase storage

--- assets/app.js
+++ assets/app.js
@@ -7,4 +7,70 @@ var config = {
     storageBucket: "",
     messagingSenderId: "688712147615"
   };
-  firebase.initializeApp(config);
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+// Button for adding trains
+$("#add-train-btn").on("click", function(event) {
+  event.preventDefault();
+
+  // Grabs input
+  var trainName = $("#train-name-input").val().trim();
+  var trainDestination = $("destination-input").val().trim();
+  var firstTrain = $("#first-train-input").val().trim();
+  var frequency = parseInt($("#frequency-input").val().trim());
+
+  // "Temporary" object for holding train data
+  var newTrain = {
+    name: trainName,
+    destination: trainDestination,
+    firstTrain: firstTrain,
+    frequency: frequency 
+  };
+
+  // Uploads employee data to the database
+  database.ref().push(newTrain);
+
+  // Logs everything to console
+  console.log(newTrain.name);
+  console.log(newTrain.destination);
+  console.log(newTrain.firstTrain);
+  console.log(newTrain.frequency);
+
+  // Clears all of the text-boxes
+  $("#train-name-input").val("");
+  $("#destination-input").val("");
+  $("#first-train-input").val("");
+  $("#frequency-input").val("");
+});
+
+// Create event for adding train to the database and a row in the html
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+  console.log(childSnapshot.val());
+
+  // Store everything in variables
+  var trainName = childSnapshot.val().name;
+  var trainDestination = childSnapshot.val().destination;
+  var firstTrain = childSnapshot.val().firstTrain;
+  var frequency = childSnapshot.val().fr;
+
+  // console.log info
+  console.log(trainName);
+  console.log(trainDestination);
+  console.log(firstTrain);
+  console.log(frequency);
+
+  // 
+  var nextArrival = moment.unix(empStart).format("MM/DD/YY");
+
+  // 
+  var minutesAway = moment().diff(moment.unix(empStart, "X"), "months");
+  console.log(empMonths);
+
+  // Add each train's data into table
+  $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" +
+  frequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
+});
